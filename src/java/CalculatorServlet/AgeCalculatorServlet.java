064d77b7f8acffc9985c8b291552cd1ee5f8ec3d
kaurr <kaurr@DESKTOP-O8AIF6C>
2023-01-27 12:34:32 -0700

    Form Created
    A form was created to enter the age of the user

---
+++ src/java/CalculatorServlet/AgeCalculatorServlet.java
@@ -0,0 +1,34 @@
+package CalculatorServlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author kaurr
+ */
+public class AgeCalculatorServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        getServletContext().getRequestDispatcher("/WEB-INF/agecalculator.jsp")
+                .forward(request, response);
+    }
+
+ 
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ web/WEB-INF/agecalculator.jsp
@@ -0,0 +1,21 @@
+<%-- 
+    Document   : agecalculator
+    Created on : 27-Jan-2023, 11:16:33 AM
+    Author     : kaurr
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Calculator</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+        <form method="post" action="Calculator" >
+            Enter your age: <input type ="number" name =" age"> <br>
+            <input type = "submit"  value ="Age next birthday">
+        </form>
+    </body>
+</html>
