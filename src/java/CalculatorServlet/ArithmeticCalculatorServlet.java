571ef1a268868e3a218b512687588fc769393ebb
kaurr <kaurr@DESKTOP-O8AIF6C>
2023-01-29 11:00:08 -0700

    Arithmetic Calculator Display All Message
    All the messages have been displayed in accordance to what the user inputs and submits.

--- src/java/CalculatorServlet/ArithmeticCalculatorServlet.java
+++ src/java/CalculatorServlet/ArithmeticCalculatorServlet.java
@@ -6,7 +6,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class ArithmeticCalculatorServlet extends HttpServlet {
+public class ArithmeticCalculatorServlet extends HttpServlet 
+{
    
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -17,7 +18,8 @@ public class ArithmeticCalculatorServlet extends HttpServlet {
       
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws ServletException, IOException 
+    {
                
         String first = request.getParameter("first");
         String second = request.getParameter("second");
@@ -31,6 +33,57 @@ public class ArithmeticCalculatorServlet extends HttpServlet {
             
         }
         
+        try {
+            int firstt = Integer.parseInt(first);
+            int secondd = Integer.parseInt(second);
+            int final_answer = 0;
+
+            request.setAttribute("first", first);
+            request.setAttribute("second", second);
+
+            if(op.equals("+")){
+                final_answer = firstt + secondd;
+
+                request.setAttribute("error", "Result: " + final_answer);
+                getServletContext().getRequestDispatcher("/WEB-INF/arithmeticcalculator.jsp")
+                        .forward(request, response);
+                return;
+            }
+            if(op.equals("-")){
+                final_answer = firstt - secondd;
+
+                request.setAttribute("error", "Result: " + final_answer);
+                getServletContext().getRequestDispatcher("/WEB-INF/arithmeticcalculator.jsp")
+                        .forward(request, response);
+                return;
+            }
+
+            if(op.equals("*")){
+                final_answer = firstt * secondd;
+
+                request.setAttribute("error", "Result: " + final_answer);
+                getServletContext().getRequestDispatcher("/WEB-INF/arithmeticcalculator.jsp")
+                        .forward(request, response);
+                return;
+            }
+
+            if(op.equals("%")){
+                final_answer = firstt  % secondd;
+
+                request.setAttribute("error", "Result: " + final_answer);
+                getServletContext().getRequestDispatcher("/WEB-INF/arithmeticcalculator.jsp")
+                        .forward(request, response);
+                return;
+            }
+        }
+        catch (Exception e){
+            
+            request.setAttribute("error", "Result: Invalid");
+                getServletContext().getRequestDispatcher("/WEB-INF/arithmeticcalculator.jsp")
+                        .forward(request, response);
+           
+        }
     }
+}
         
- }
+
